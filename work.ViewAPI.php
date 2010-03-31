f6bce10b74137137fc7aa034af07cf92ee7337df
= <joohong.kim@access-company.com>
2010-03-31 15:55:58 +0900

    View api file added

---
+++ work.ViewAPI.php
@@ -0,0 +1,26 @@
+<?php
+		collapse_open( 'Work' );
+?>
+<br/>
+<a name="works"/></a>
+<table class="width100" cellspacing="1">
+
+<tr>
+	<td class="form-title"><?php collapse_icon( 'Work' ); echo 'Related Works' ?></td>
+</tr>
+		<!-- ?php Source_View_Changesets( $t_changesets ); ?-->
+</table>
+<?php
+			collapse_closed( 'Work' );
+?>
+<br/>
+<table class="width100" cellspacing="1">
+
+<tr>
+	<td class="form-title"><?php collapse_icon( 'Work' ); echo 'Related Works' ?></td>
+</tr>
+
+</table>
+<?php
+	collapse_end('Work');
+?>
