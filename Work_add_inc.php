b4da56c07ef95b882d662b711255faf20a4c9b80
= <joohong.kim@access-company.com>
2010-04-14 22:14:50 +0900

    work add

--- Work_add_inc.php
+++ Work_add_inc.php
@@ -1,3 +1,70 @@
+<?php 
+function worktype_get_all_rows( $p_project_id, $p_inherit = null, $p_sort_by_project = false ) {
+	$c_project_id = db_prepare_int( $p_project_id );
+
+	$t_worktype_table = plugin_table( 'type' );
+	$t_project_table = db_get_table( 'mantis_project_table' );
+
+	if ( $c_project_id == ALL_PROJECTS ) {
+		$t_inherit = false;
+	} else {
+		if ( $p_inherit === null ) {
+			$t_inherit = config_get( 'subprojects_inherit_categories' );
+		} else {
+			$t_inherit = $p_inherit;
+		}
+	}
+
+	if ( $t_inherit ) {
+		$t_project_ids = project_hierarchy_inheritance( $p_project_id );
+		$t_project_where = ' project_id IN ( ' . implode( ', ', $t_project_ids ) . ' ) ';
+	} else {
+		$t_project_where = ' project_id=' . $p_project_id . ' ';
+	}
+
+	$query = "SELECT t.*, p.name AS project_name FROM $t_worktype_table AS t
+				LEFT JOIN $t_project_table AS p
+					ON t.project_id=p.id
+				WHERE $t_project_where
+				ORDER BY t.name ";
+	$result = db_query_bound( $query );
+	$count = db_num_rows( $result );
+	$rows = array();
+	for( $i = 0;$i < $count;$i++ ) {
+		$row = db_fetch_array( $result );
+
+		$rows[] = $row;
+	}
+
+	return $rows;
+}
+
+function print_worktype_option_list( $p_worktype_id = 0, $p_project_id = null ) {
+	$t_worktype_table = db_get_table( 'mantis_category_table' );
+	$t_project_table = db_get_table( 'mantis_project_table' );
+
+	if( null === $p_project_id ) {
+		$t_project_id = helper_get_current_project();
+	} else {
+		$t_project_id = $p_project_id;
+	}
+
+	if( 0 == $p_worktype_id ) {
+		echo "<option value=\"0\"", check_selected( $p_worktype_id, 0 ), '>';
+		echo string_attribute( lang_get( 'select_option' ) ), '</option>';
+	}
+
+	$worktype_arr = worktype_get_all_rows( $t_project_id, /* inherit */ null, /* sortByProject */ true );
+
+	foreach( $worktype_arr as $t_worktype_row ) {
+		$t_worktype_id = $t_worktype_row['id'];
+		echo "<option value=\"$t_worktype_id\"";
+		check_selected( $p_worktype_id, $t_worktype_id );
+		echo '>' . string_attribute( category_full_name( $t_worktype_id, $t_worktype_row['project_id'] != $t_project_id ) ) . '</option>';
+	}
+}
+?>
+
 <a name="addbugnote"></a> <br />
 
 <?php
@@ -18,11 +85,11 @@
 		Work
 	</td>
 	<td width="50%">
-		Jobs: <select>
-			<option>Research</option>
-			<option>Merge</option>
-			<option>Implementation</option>
-			<option>Debugging</option>		
+		Jobs: 
+		<select name="worktype_id">
+				<?php
+					print_worktype_option_list( 0 );
+				?>
 		</select>
 	</td>
 	<td width="25%">
